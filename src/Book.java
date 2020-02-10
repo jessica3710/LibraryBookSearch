372c42776598705b8504d7158aac2ef1f584845c
Jessica Zheng <jessicazheng3710@gmail.com>
2020-02-10 01:02:20 -0500

    Upload of Library Book Search Project
    This allows the user to put a file in and search for either a number,
    title or author from the list. It is then displayed in the text field.

---
+++ src/Book.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jessica
+ */
+public class Book {
+    //initialized variables for the ArrayList
+    private int refNumber;
+    private String title;
+    private String author;
+    
+    //getters and setters for the variables
+    public int getRefNumber() {
+        return refNumber;
+    }
+
+    public void setRefNumber(int refNumber) {
+        this.refNumber = refNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    
+    
+    
+}
